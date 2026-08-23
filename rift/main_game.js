4c6661ebf1c0a04ff0f681baaedf624c6d39f484
nicholasgearinger-code <nicholasgearinger@gmail.com>
2026-08-23 16:29:37 -0400

    Route Rift through reference-guided volumetric clouds

--- rift/main_game.js
+++ rift/main_game.js
@@ -43,8 +43,8 @@ if (!source.includes(editsLoopMarker)) {
 const extraEdits = [
   [
     'import { createVolumetricClouds, updateVolumetricClouds } from "./volumetricClouds.js";',
-    'import { createVolumetricClouds, updateVolumetricClouds } from "./volumetricClouds_temporal_v4.js";',
-    "temporal cloud renderer import",
+    'import { createVolumetricClouds, updateVolumetricClouds } from "./volumetricClouds_reference_guided.js";',
+    "reference-guided temporal cloud renderer import",
   ],
   [
     "tempWaterGlintDir.copy(moonLight.position).lerp(sun.position, dayNight.dayAmount);",

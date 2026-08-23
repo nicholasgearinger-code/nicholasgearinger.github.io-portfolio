8e3fd8dc37e4651cd4d89abffcbfbb28274b0345
nicholasgearinger-code <nicholasgearinger@gmail.com>
2026-08-23 19:25:23 -0400

    Fix GLSL cloud shader world-space vertex position

--- rift/shaders/clouds_perlin_worley.vert.glsl
+++ rift/shaders/clouds_perlin_worley.vert.glsl
@@ -3,13 +3,14 @@ precision highp float;
 
 layout(location = 0) in vec3 position;
 
+uniform mat4 modelMatrix;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
 out vec3 vWorldPosition;
 
 void main() {
-  vec4 world = vec4(position, 1.0);
+  vec4 world = modelMatrix * vec4(position, 1.0);
   vWorldPosition = world.xyz;
-  gl_Position = projectionMatrix * modelViewMatrix * world;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }

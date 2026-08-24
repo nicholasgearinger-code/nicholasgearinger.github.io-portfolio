376c2cd04b584fcfe3ac591564497f8d6ec9c467
nicholasgearinger-code <nicholasgearinger@gmail.com>
2026-08-23 21:12:55 -0400

    Route clouds through fluffy Perlin-Worley v2

--- rift/volumetricClouds_reference_v2.js
+++ rift/volumetricClouds_reference_v2.js
@@ -1,8 +1,8 @@
 // Compatibility entry point retained because main_game.js already imports this
 // module. The live cloud implementation is now the true 3D Perlin-Worley
-// raymarcher in volumetricClouds_perlinWorley.js.
+// raymarcher with the v2 large-cumulus presentation tuning.
 export {
   createVolumetricClouds,
   updateVolumetricClouds,
   disposeVolumetricClouds,
-} from "./volumetricClouds_perlinWorley.js";
+} from "./volumetricClouds_perlinWorley_v2.js";

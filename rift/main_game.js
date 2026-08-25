4a405ff7fcd7fe8961a04b9d226d3c92f9d74426
nicholasgearinger-code <nicholasgearinger@gmail.com>
2026-08-24 21:19:57 -0400

    Activate photographic sunrise sunset lighting v8

--- rift/main_game.js
+++ rift/main_game.js
@@ -51,8 +51,8 @@ const extraEdits = [
   ],
   [
     'import { createDayNightCycle, updateDayNightCycle, CYCLE_SECONDS } from "./dayNightCycle.js";',
-    'import { createDayNightCycle, updateDayNightCycle, CYCLE_SECONDS } from "./dayNightCycle_celestial_physical_v7.js";',
-    "photometric cloud-aware global solar lighting v7 import",
+    'import { createDayNightCycle, updateDayNightCycle, CYCLE_SECONDS } from "./dayNightCycle_celestial_physical_v8.js";',
+    "photographic sunrise sunset global solar lighting v8 import",
   ],
   [
     "sceneBackgroundColor.copy(dayNight.skyHorizon).lerp(dayNight.skyZenith, 0.5);",

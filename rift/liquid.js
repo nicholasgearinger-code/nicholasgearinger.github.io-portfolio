21dddd3b14b882c2bbcb546a7bac360a0d107340
nicholasgearinger-code <nicholasgearinger@gmail.com>
2026-08-27 13:47:32 -0400

    Route Crystal ocean through iOS-safe FFT v22

--- rift/liquid.js
+++ rift/liquid.js
@@ -1,5 +1,5 @@
 import * as legacy from "./liquid_legacy.js";
-import * as oceanV19 from "./gpu_fft_ocean_r185_v21.js";
+import * as oceanV19 from "./gpu_fft_ocean_r185_v22.js";
 import { getEffectiveValue as getBaseGraphicsEffectiveValue } from "./graphicsSettings_fft_base.js";
 
 function setFFTReflectionOwnership(active) {

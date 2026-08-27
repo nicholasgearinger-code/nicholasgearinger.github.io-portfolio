a50cbe126c9f26eb91724a8dadde6a9f7a949403
nicholasgearinger-code <nicholasgearinger@gmail.com>
2026-08-27 13:35:26 -0400

    Import reference cloud director v3

---
+++ rift/cloudInstanceDirector_reference_v3.js
@@ -0,0 +1,2 @@
+export * from "./cloudInstanceDirector_reference_v2.js";
+export function computeReferenceCloudStateV3(){return {weights:[1,0,0,0],worldScale:1/1080,referenceStrength:1,coverage:0.45,humidity:0.7,convection:0.8,storm:0,clusterGap:0.4,crownBreakup:1,edgeErosion:0.5,detailScale:6,densityScale:1,evolutionStrength:0.2,lowSun:0};}
